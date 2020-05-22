26a98fbe4c1b7e16bfa154978c82d7d96dcccd4c
Deep Patel <deeppatel3543@gmail.com>
2020-05-22 17:53:59 +1000

    Profile Button Overlay Design & Few more tweaks

--- src/Profile.js
+++ src/Profile.js
@@ -25,93 +25,102 @@ export default class Profile extends Component {
       currentDocument:[],
 
       timestamp: '',
-      isLoading: false
+      isLoading: false,
+      showing: false
     };
 
   }
 
   render() {
-    const { handleSignOut, userSession } = this.props
+    const { handleSignOut, userSession } = this.props;
     const { person } = this.state;
-    
+    const { showing } = this.state;
+
     return (
       !userSession.isSignInPending() ?
 
       <div classname="mainsection">
-      <div className="panel-welcome" id="section-2">
-        <div className="avatar-section">
-          <img src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage } className="img-rounded avatar" id="avatar-image" alt=""/>
-          <div className="username">
-            <h4>
-              <span id="heading-name">{ person.name() ? person.name()
-                : 'Nameless Person'}</span>
-            </h4>
-          <span>{this.state.username}</span>
-          </div>
-      </div>
-        <p className="lead">
-          <button
-            className="btn btn-primary btn-lg"
-            id="signout-button"
-            onClick={ handleSignOut.bind(this) }>
-            Logout
-          </button>
-        </p>
-      </div> 
+        <div className="profile-container">
+          <a className="button" onClick={() => this.setState({ showing: !showing })}>
+            <img src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage } className="profile-button" id="avatar-image" alt=""/>
+          </a>
+          <div id="popup1" className="overlay" style={{display: (showing ? 'block' : 'none')}}>
+              <div className="panel-welcome" id="section-2">
+                <div className="avatar-section">
+                  <img src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage } className="img-rounded avatar" id="avatar-image" alt=""/>
+                  <div className="username">
+                    <h4>
+                      <span id="heading-name">{ person.name() ? person.name()
+                        : 'Nameless Person'}</span>
+                    </h4>
+                  <span>{this.state.username}</span>
+                  </div>
+                </div>
+                <p className="lead">
+                  <button
+                    className="btn btn-primary btn-lg"
+                    id="signout-button"
+                    onClick={ handleSignOut.bind(this) }>
+                    SignOut
+                    </button>
+                </p>
+              </div>
+          </div> 
+        </div>
+        
+        <div className="work-space">
+          <h2>
+            Welcome to PaperState!
+          </h2>
+          <p>
+            Lasted updated: {this.state.currentDocument.created_at ? new Date(this.state.currentDocument.created_at).toLocaleString('en-US', {
+              weekday: 'short',
+              month: 'short',
+              day: 'numeric',
+              year: 'numeric',
+              hour: 'numeric',
+              minute: 'numeric',
+              second: 'numeric',
+              hour12: true,
+              timeZoneName: 'short'
+            }) : "No File yet"}
+          </p>
+        </div>
 
-          <div className="work-space">
-            <h2>
-              Welcome to PaperState!
-            </h2>
-            <p>
-              Lasted updated: {this.state.currentDocument.created_at ? new Date(this.state.currentDocument.created_at).toLocaleString('en-US', {
-                weekday: 'short',
-                month: 'short',
-                day: 'numeric',
-                year: 'numeric',
-                hour: 'numeric',
-                minute: 'numeric',
-                second: 'numeric',
-                hour12: true,
-                timeZoneName: 'short'
-              }) : "No File yet"}
-            </p>
-          </div>
-
-          <div id="section-3">
+        <div id="section-3">
+          
+          <div id="Text-section">
             
-            <div id="Text-section">
-              
-              <div id="Fill-in-text">
-                <textarea className="input-status"
-                  placeholder="Document text goes here"
-                  onChange={e=>this.handleChange(e)}
-                  value={this.state.newText}
+            <div id="Fill-in-text">
+              <textarea className="input-status"
+                placeholder="Document text goes here"
+                onChange={e=>this.handleChange(e)}
+                value={this.state.newText}
 
-                  />
-              </div>
+                />
+            </div>
 
-              <div id="Formatted-text">
-                <textarea className="input-status2"
-                  placeholder="formatted text goes here"
-                  disabled='disabled'
-                  value={this.state.currentDocument.text}>
-                  </textarea>
-              </div>
-              
-              <div id="Submit-button">
-                <button className="btn btn-success btn-lg"
-                onClick={e => this.saveNewText(e)}
-                >
-                Submit Changes
-                </button>
-              </div>
-
+            <div id="Formatted-text">
+              <textarea className="input-status2"
+                placeholder="formatted text goes here"
+                disabled='disabled'
+                value={this.state.currentDocument.text}>
+                </textarea>
+            </div>
+            
+            <div id="Submit-button">
+              <button className="btn btn-success btn-lg"
+              onClick={e => this.saveNewText(e)}
+              >
+              Submit Changes
+              </button>
             </div>
 
           </div>
 
-        </div>:null
+        </div>
+
+      </div>:null
     );
   }
 
@@ -159,5 +168,5 @@ export default class Profile extends Component {
 
   handleChange(event) {
    this.setState({newText: event.target.value});
-    }
+  }
 }
